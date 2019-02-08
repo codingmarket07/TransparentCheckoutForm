07e7d1c767b1090be7c87e824f3947ddb6e43d48
rajeshdn <codingmarket@gmail.com>
2019-02-08 20:11:42 +0530

    Files readded

--- vanilla-card-validator-master/src/app.js
+++
@@ -1,5 +0,0 @@
-import * as cardValidator from './card-validator';
-
-cardValidator.defaults.querySelectors.cardNumber = '#card';
-cardValidator.defaults.querySelectors.logo = '#logo';
-cardValidator.init();
